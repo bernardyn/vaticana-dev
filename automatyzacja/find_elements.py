c07944656703fa3c3fa86f60c2f1efbfb5874686
Andrzej Woloszkiewicz <andrzej.woloszkiewicz@gft.com>
2023-05-06 22:52:03 +0200

    insert text and attributes

--- automatyzacja/find_elements.py
+++ automatyzacja/find_elements.py
@@ -3,25 +3,48 @@ import lxml.etree as ET
 # open the input file
 input = ET.parse('nr 1.xml')
 
-# xpath variables
-idDocument_xpath = '//p[@rend="Title"]'
-placeName_xpath = '//hi[@rend="<placeName>_Znak"]'
-dateDoc_xpath = '//hi[@rend="<date>_Znak"]'
-abstract_xpath = '//p[@rend="abstract"]'
+# output template
+templateXml = ET.parse('template.xml')
 
 # element finding by xpath
-def findElementByXpath(xpath):
-    return input.find(xpath).xpath("string()")
+def findElementByXpath(source, xpath):
+    return source.find(xpath).xpath("string()")
 
-# variables to
-idDocument = findElementByXpath(idDocument_xpath)
-placeName = findElementByXpath(placeName_xpath)
-dateDoc = findElementByXpath(dateDoc_xpath)
-abstract = findElementByXpath(abstract_xpath)
+# find and change text in output file
+def insertText(xpath, newText):
+    templateXml.find(xpath).text = newText
 
-#printing
-print(abstract)
+def insertAtrribut(xpath, newAttrName, newAttrValue):
+    templateXml.find(xpath).set(newAttrName, newAttrValue)
+
+# input variables
+idDocument = findElementByXpath(input, '//p[@rend="Title"]')
+placeName = findElementByXpath(input, '//hi[@rend="<placeName>_Znak"]')
+dateDoc = findElementByXpath(input, '//hi[@rend="<date>_Znak"]')
+abstract = findElementByXpath(input, '//p[@rend="abstract"]')
+
+# xpath output variables
+output_title_xpath = '//seriesStmt/title'
+output_placeName_xpath = '//creation/placeName'
+output_abstract_xpath = '//profileDesc/abstract'
+output_date_xpath = '//creation/date'
+
+# set title
+insertText(output_title_xpath, idDocument)
+
+# set place name
+insertText(output_placeName_xpath, placeName)
+if(placeName == 'Roma'):
+    insertAtrribut(output_placeName_xpath, 'ana', 'Rzym')
+else:
+    print('placeName to nie Roma')
+
+# set abstract
+insertText(output_abstract_xpath, abstract)
+
+# set date
+insertText(output_date_xpath, dateDoc)
 
 
-
-#print(ET.tostring(tag, pretty_print=True, encoding="unicode"))
+# printing output
+print(ET.tostring(templateXml, pretty_print=True, encoding="unicode"))
